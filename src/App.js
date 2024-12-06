19331a0a921e6043ac7a0f2151a79faef7df144e
Joe <V.josephraj26@gmail.com>
2024-12-06 15:10:55 -0500

    Added the ToDo List application

--- src/App.js
+++ src/App.js
@@ -3,41 +3,55 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-  function addOne() {
-    setCount(count + 1);
+  const [id, setId] = useState(0);
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
+
+  function addTask() {
+    if (taskInput !== '') {
+      tasks.push(taskInput);
+      console.log(tasks)
+    }
+    setTaskInput('');
   }
 
-  function minusOne() {
-    setCount(count - 1);
+  function removeTask(index) {
+    setTasks(tasks.filter((_, i) => i !== index));
   }
 
-  function resetCount() {
-    setCount(0);
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      addTask();
+    }
   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Ok!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <br></br>
-        <div>
-          <button className='button' onClick={addOne}>+</button>
-          <button className='button' onClick={minusOne}>-</button>
-          <button className='button' onClick={resetCount}>Reset</button>
+    <div className='App bg-charcoal flex justify-center items-center h-screen'>
+      <div className='w-1/2'>
+        <div className='flex flex-col bg-carmine rounded-2xl py-5'>
+          
+          <div className='flex items-center px-4'>
+            <h1 className='text-5xl text-jasmine'>To-Do List</h1>
+          </div>
+
+          <div className='flex justify-center mt-10 mb-6'>
+            <div className='rounded-full bg-white w-[80%]'>
+              <input placeholder='Enter Task' value={taskInput} onChange={(e) => setTaskInput(e.target.value)} onKeyDown={handleKeyDown} className='rounded-l-full w-3/4 px-4 py-2' />
+              <button onClick={addTask} className='border bg-emerald text-charcoal font-bold rounded-full w-1/4 py-2'>ADD</button>
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-2 px-5 items-center'>
+            {tasks.map((task, index) => (
+              <div key={index} className='flex gap-2 w-[85%]'>
+                <button onClick={() => removeTask(index)} className='bg-emerald rounded-full px-2'>&#10004;</button>
+                <div className='text-jasmine'>{task}</div>
+              </div>
+            ))}
+          </div>
+
         </div>
-        <div className='count'>Count: {count}</div>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    
+    extend: {
+      colors: {
+        'charcoal': '#414750',
+        'jasmine': '#F4D06F',
+        'carmine': '#8C001A',
+        'emerald': '#06D6A0',
+      },
+    },
+  },
+  plugins: [],
+}
+
